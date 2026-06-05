e83dd192970fe3f7a5421aefb9331d1960709297
hermaeusmora <aleshka.dev@gmail.com>
2026-06-05 23:21:12 +0300

    feat: setup typeorm;

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfig } from './shared/configs/typerorm.config';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        typeormConfig(configService),
+    }),
+  ],
 })
 export class AppModule {}
