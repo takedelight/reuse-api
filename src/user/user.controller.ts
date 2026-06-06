440f4d799bc7b395862f567c89c7516b5071ebb6
hermaeusmora <aleshka.dev@gmail.com>
2026-06-06 19:39:56 +0300

    feat: add user controller with  tests

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,43 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { UserResponseDto } from './dto/user-response.dto';
 import { UserService } from './user.service';
 
-@Controller('user')
+@Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getAllUsers(): Promise<UserResponseDto[]> {
+    return this.userService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string): Promise<UserResponseDto | null> {
+    return this.userService.getUserById(id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
+    return this.userService.createUser(createUserDto);
+  }
+
+  @Patch(':id')
+  updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<UserResponseDto> {
+    return this.userService.updateUser(id, updateUserDto);
+  }
 }
