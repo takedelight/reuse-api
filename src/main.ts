6a63833aff9337c24d9d145a47726e7b88c6063e
hermaeusmora <aleshka.dev@gmail.com>
2026-06-06 11:13:27 +0300

    feat: add CORS settings; add helmet protection

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
+import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
 
   const APP_PORT = configService.getOrThrow<number>('PORT');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.setGlobalPrefix('api');
+
+  app.use(helmet());
+
+  app.enableCors({
+    origin: configService.getOrThrow<string>('CORS_ORIGIN'),
+    credentials: true,
+    methods: ['GET', 'PATCH', 'POST', 'DELETE'],
+  });
 
   await app.listen(APP_PORT, () =>
     console.log(`Application is running on port http://localhost:${APP_PORT}`),
